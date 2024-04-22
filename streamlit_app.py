94d82b05343bfdf45dbf6483fbf5dc584422356e
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-21 22:42:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,48 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from scipy.stats import poisson, norm, chisquare
+import requests
+
+def load_data(file_url):
+    response = requests.get(file_url)
+    if response.status_code == 200:
+        data = response.content.decode('utf-8').split('\n')
+        return [int(value) for value in data if value]
+    else:
+        return None
 
 def main():
     st.title("Poisson and Gaussian Distribution Adjustments")
 
-    # Upload data files
-    data1 = st.file_uploader("Upload data1.csv", type="csv")
-    data2 = st.file_uploader("Upload data2.csv", type="csv")
+    # GitHub repository URL
+    repo_url = "https://raw.githubusercontent.com/your_username/your_repo/main/"
+
+    # Load data from GitHub
+    data1_url = repo_url + "data1.csv"
+    data2_url = repo_url + "data2.csv"
+
+    data1 = load_data(data1_url)
+    data2 = load_data(data2_url)
 
     if data1 and data2:
-        # Read data as strings
-        data1 = pd.read_csv(data1, names=['decays_air'], header=None, dtype=str)
-        data2 = pd.read_csv(data2, names=['decays_cesium'], header=None, dtype=str)
-
-        # Convert to numeric
-        data1['decays_air'] = data1['decays_air'].apply(lambda x: int(x))
-        data2['decays_cesium'] = data2['decays_cesium'].apply(lambda x: int(x))
+        data1 = pd.Series(data1)
+        data2 = pd.Series(data2)
 
         # Poisson distribution adjustment
-        lambda_air = data1['decays_air'].mean()
-        lambda_cesium = data2['decays_cesium'].mean()
+        lambda_air = data1.mean()
+        lambda_cesium = data2.mean()
 
-        poisson_air_chisq = chisquare(data1['decays_air'], poisson.pmf(data1['decays_air'], lambda_air))
-        poisson_cesium_chisq = chisquare(data2['decays_cesium'], poisson.pmf(data2['decays_cesium'], lambda_cesium))
+        poisson_air_chisq = chisquare(data1, poisson.pmf(data1, lambda_air))
+        poisson_cesium_chisq = chisquare(data2, poisson.pmf(data2, lambda_cesium))
 
         # Gaussian distribution adjustment
-        mu_air = data1['decays_air'].mean()
-        sigma_air = data1['decays_air'].std()
-        mu_cesium = data2['decays_cesium'].mean()
-        sigma_cesium = data2['decays_cesium'].std()
+        mu_air = data1.mean()
+        sigma_air = data1.std()
+        mu_cesium = data2.mean()
+        sigma_cesium = data2.std()
 
-        gaussian_air_chisq = chisquare(data1['decays_air'], norm.pdf(data1['decays_air'], mu_air, sigma_air))
-        gaussian_cesium_chisq = chisquare(data2['decays_cesium'], norm.pdf(data2['decays_cesium'], mu_cesium, sigma_cesium))
+        gaussian_air_chisq = chisquare(data1, norm.pdf(data1, mu_air, sigma_air))
+        gaussian_cesium_chisq = chisquare(data2, norm.pdf(data2, mu_cesium, sigma_cesium))
 
         # Display results
         st.header("Poisson Distribution Adjustment")
@@ -56,7 +66,8 @@ def main():
             st.write("For the cesium data, the Gaussian distribution adjustment provides a better fit.")
 
     else:
-        st.warning("Please upload both data1.csv and data2.csv files.")
+        st.warning("Error loading data from GitHub.")
 
 if __name__ == "__main__":
     main()
+    
