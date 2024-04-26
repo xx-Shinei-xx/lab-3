e62d520fce42e8ecc1677843de0f0ea71c764f5f
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-25 20:21:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,5 @@ def main():
     # Plot distribution based on selection
     plot_distribution(data['Value'], distribution_type, parameter_value)
 
-#if __name__ == '__main__':
- #   main()
+if __name__ == '__main__':
+   main()
