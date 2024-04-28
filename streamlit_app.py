376e43fef99384b25c979800c6f427602809ce93
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-27 19:31:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,10 +66,11 @@ elif selected_option == 'Data1':
 
     st.subheader('Distribución de Gauss:')
     st.write("Distribución de Gauss para el conjunto de datos 'data1.csv'.")
-     data1 = np.genfromtxt('data1.csv', delimiter=',', skip_header=1, usecols=1)
-        fig = plot_gaussian_distribution(data1, 'Distribución de Gauss - Data1')
-        st.plotly_chart(fig)
+     st.plotly_chart(fig)
     if st.button('Mostrar Tabla y Valor de Chi Cuadrado (Gaussiana)'):
+        data1 = np.genfromtxt('data1.csv', delimiter=',', skip_header=1, usecols=1)
+        fig = plot_gaussian_distribution(data1, 'Distribución de Gauss - Data1')
+        
         mostrar_tabla_y_chi(data1)
         st.markdown("---")
 
