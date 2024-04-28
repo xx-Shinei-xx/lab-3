742fdae9fc7317d449dc2e861491650dbbe8030e
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-04-27 19:06:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,36 +4,9 @@ import pandas as pd
 import plotly.graph_objects as go
 from scipy.stats import norm, poisson, chisquare
 
-# Cargar los datos 1 y 2
-data1 = np.genfromtxt('data1.csv', delimiter=',', skip_header=1, usecols=1)
-data2 = np.genfromtxt('data2.csv', delimiter=',', skip_header=1, usecols=1)
-
-# distribución gaussiana
-def plot_gaussian_distribution(data):
-    mu, sigma = np.mean(data), np.std(data)
-    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-    y = norm.pdf(x, mu, sigma)
-    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines', name='Distribución Gaussiana'))
-    fig.add_trace(go.Histogram(x=data, histnorm='probability density', name='Histograma Gaussiano'))
-    fig.update_layout(title='Distribución Gaussiana', xaxis_title='Valor', yaxis_title='Densidad de probabilidad')
-    st.plotly_chart(fig)
-
-
-
-
-# distribución de Poisson
-def plot_poisson_distribution(data):
-    mu = np.mean(data)
-    x = np.arange(0, max(data) + 1)
-    y = poisson.pmf(x, mu)
-    fit_y = poisson.pmf(x, mu)
-    fig = go.Figure(data=[go.Bar(x=x, y=y, name='Distribución de Poisson'),
-                          go.Scatter(x=x, y=fit_y, mode='lines', name='Ajuste de Poisson', line=dict(color='red', width=2))])
-    fig.update_layout(title='Distribución de Poisson', xaxis_title='Valor', yaxis_title='Probabilidad')
-    st.plotly_chart(fig)
-
-#-------------------
-
+# Función para calcular chi cuadrado
+def calcular_chi_cuadrado(f_obs, f_exp):
+    return np.sum((f_obs - f_exp)**2 / f_exp)
 
 # Función para calcular frecuencias observadas y esperadas
 def calcular_frecuencias(data):
@@ -48,10 +21,31 @@ def mostrar_tabla(data):
     valores_unicos, frecuencia_observada, frecuencia_esperada = calcular_frecuencias(data)
     tabla_data = {"Valor": valores_unicos, "Frecuencia Observada": frecuencia_observada, "Frecuencia Esperada": frecuencia_esperada}
     tabla = st.table(tabla_data)
-# Función para calcular y mostrar la distribución de Poisson
-def plot_poisson_distribution(data):
-    mu = np.mean(data)
-    x = np.arange(0, max(data) + 1)
+    chi_cuadrado = calcular_chi_cuadrado(frecuencia_observada, frecuencia_esperada)
+    st.write(f"Valor de chi cuadrado: {chi_cuadrado}")
+
+# Streamlit
+st.title('Análisis de Datos')
+
+# Botón para seleccionar el conjunto de datos
+selected_data = st.radio('Seleccionar conjunto de datos:', ('data1', 'data2'))
+
+if selected_data == 'data1':
+    st.subheader('Distribuciones en el decaimiento solo con el aire')
+    st.subheader('Distribución de Gauss:')
+    # Cargar datos
+    data1 = np.genfromtxt('data1.csv', delimiter=',', skip_header=1, usecols=1)
+    mu, sigma = np.mean(data1), np.std(data1)
+    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+    y = norm.pdf(x, mu, sigma)
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines', name='Distribución Gaussiana'))
+    fig.add_trace(go.Histogram(x=data1, histnorm='probability density', name='Histograma Gaussiano'))
+    fig.update_layout(title='Distribución Gaussiana', xaxis_title='Valor', yaxis_title='Densidad de probabilidad')
+    st.plotly_chart(fig)
+
+    st.subheader('Distribución de Poisson:')
+    mu = np.mean(data1)
+    x = np.arange(0, max(data1) + 1)
     y = poisson.pmf(x, mu)
     fit_y = poisson.pmf(x, mu)
     fig = go.Figure(data=[go.Bar(x=x, y=y, name='Distribución de Poisson'),
@@ -59,54 +53,31 @@ def plot_poisson_distribution(data):
     fig.update_layout(title='Distribución de Poisson', xaxis_title='Valor', yaxis_title='Probabilidad')
     st.plotly_chart(fig)
 
-# Función para calcular chi cuadrado
-def calcular_chi_cuadrado(f_obs, f_exp):
-    return np.sum((f_obs - f_exp)**2 / f_exp)
-
-
-
-#-----------------------------------
-
-# Streamlit
-st.title('Análisis de Datos')
-
-# Botón para seleccionar el conjunto de datos
-selected_data = st.radio('Seleccionar conjunto de datos:', ('data1.csv', 'data2.csv'))
-
-if selected_data == 'data1.csv':
-    st.subheader('Distribuciones en el decaimiento solo con el aire')
-    st.subheader('Distribución de Gauss:')
-    plot_gaussian_distribution(data1)
-    #if st.button('Mostrar Tabla'):  
-        
-    st.subheader('Distribución de Poisson:')
-    plot_poisson_distribution(data1)
     if st.button('Mostrar Tabla'):
         mostrar_tabla(data1)
-        valores_unicos, frecuencia_observada, frecuencia_esperada = calcular_frecuencias(data1)
-        chi_cuadrado = calcular_chi_cuadrado(frecuencia_observada, frecuencia_esperada)
-        st.write(f"Valor de chi cuadrado: {chi_cuadrado}")
 
-
-
-
-elif selected_data == 'data2.csv':
+elif selected_data == 'data2':
     st.subheader('Distribuciones en el decaimiento del cesio-137')
     st.subheader('Distribución de Gauss:')
-    plot_gaussian_distribution(data2)
-    # if st.button('Mostrar Tabla'):
-        
+    # Cargar datos
+    data2 = np.genfromtxt('data2.csv', delimiter=',', skip_header=1, usecols=1)
+    mu, sigma = np.mean(data2), np.std(data2)
+    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+    y = norm.pdf(x, mu, sigma)
+    fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines', name='Distribución Gaussiana'))
+    fig.add_trace(go.Histogram(x=data2, histnorm='probability density', name='Histograma Gaussiano'))
+    fig.update_layout(title='Distribución Gaussiana', xaxis_title='Valor', yaxis_title='Densidad de probabilidad')
+    st.plotly_chart(fig)
+
     st.subheader('Distribución de Poisson:')
-    plot_poisson_distribution(data2)
+    mu = np.mean(data2)
+    x = np.arange(0, max(data2) + 1)
+    y = poisson.pmf(x, mu)
+    fit_y = poisson.pmf(x, mu)
+    fig = go.Figure(data=[go.Bar(x=x, y=y, name='Distribución de Poisson'),
+                          go.Scatter(x=x, y=fit_y, mode='lines', name='Ajuste de Poisson', line=dict(color='red', width=2))])
+    fig.update_layout(title='Distribución de Poisson', xaxis_title='Valor', yaxis_title='Probabilidad')
+    st.plotly_chart(fig)
+
     if st.button('Mostrar Tabla'):
         mostrar_tabla(data2)
-        valores_unicos, frecuencia_observada, frecuencia_esperada = calcular_frecuencias(data2)
-        chi_cuadrado = calcular_chi_cuadrado(frecuencia_observada, frecuencia_esperada)
-        st.write(f"Valor de chi cuadrado: {chi_cuadrado}")
-
-
-
-
-
-
-
